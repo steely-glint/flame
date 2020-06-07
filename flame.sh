25444b29961c35f0d6d7b126ab87435dc2e0ff94
T H Panton <thp@westhawk.co.uk>
2020-06-07 18:44:41 +0100

    local dir

--- flame.sh
+++ flame.sh
@@ -2,4 +2,4 @@
 
 home=session.html
 
-jjs -J-Xmx256m -J-Dpe.pi.client.small.defaultPage=${home} -J-Djava.net.preferIPv4Stack=true -cp ~/pipe-java-client-current.jar flame.js
+jjs -J-Xmx256m -J-Dpe.pi.client.small.defaultPage=${home} -J-Djava.net.preferIPv4Stack=true -cp pipe-java-client-current.jar flame.js
