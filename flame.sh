26b9102610cb3134bc9e898d744d3d9876996107
T H Panton <thp@westhawk.co.uk>
2020-06-08 11:52:54 +0100

    And the startup script

--- flame.sh
+++ flame.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-home=session.html
+home=install.html
 
 jjs -J-Xmx256m -J-Dpe.pi.client.small.defaultPage=${home} -J-Djava.net.preferIPv4Stack=true -cp pipe-java-client-current.jar flame.js
