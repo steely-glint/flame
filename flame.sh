627c5cd2bf8d947c6da7c335b83e98d125d67b7a
T H Panton <thp@westhawk.co.uk>
2020-06-08 12:14:29 +0100

    added newstyle config

--- flame.sh
+++ flame.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-home=install.html
+home=install.html?target=index.html
 
 jjs -J-Xmx256m -J-Dpe.pi.client.small.defaultPage=${home} -J-Djava.net.preferIPv4Stack=true -cp pipe-java-client-current.jar flame.js
